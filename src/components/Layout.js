42303b276eac53e91fdde99fe0b2d9545712dade
rochmadnf <rochmadnf@gmail.com>
2022-10-27 21:12:21 +0800

    middleware

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,27 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useRecoilValueLoadable } from "recoil";
+import { authUserState } from "../store/authentication";
 import Navbar from "./Navbar";
 
-export default function Layout({ title, children }) {
+export default function Layout({ title, children, middleware = "guest" }) {
+  const { replace } = useRouter();
+  const authUser = useRecoilValueLoadable(authUserState);
+
+  useEffect(() => {
+    if (
+      middleware === "guest" &&
+      authUser.state === "hasValue" &&
+      authUser.contents
+    ) {
+      replace("/dashboard");
+    }
+
+    if (middleware === "auth" && authUser.contents == null) {
+      replace("/login");
+    }
+  }, [authUser.contents]);
   return (
     <div>
       <Head>
--- src/pages/settings/profile.js
+++ src/pages/settings/profile.js
@@ -1,10 +1,12 @@
-import { Container, Layout } from "../components";
+import { Container, Layout } from "../../components";
 
-export default function Dashboard() {
+export default function ProfilePage() {
   return (
-    <Layout middleware="auth" title="Dashboard">
+    <Layout middleware="auth" title="Update Profile Information">
       <Container>
-        <h1 className="text-xl font-semibold mb-2">Dashboard</h1>
+        <h1 className="text-xl font-semibold mb-2">
+          Update Profile Information
+        </h1>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Et fugit
         voluptatibus maiores esse porro facere nihil sit nobis aut autem id
         consectetur, at architecto, voluptates, ut voluptas! Amet, saepe fugiat.
